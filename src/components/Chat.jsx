82034a0fa3efd694e72f174b3b73477eb3dc5592
bouabre225 <angebouabre463@gmail.com>
2025-11-12 23:55:46 +0100

    ajout du composent chat

---
+++ src/components/Chat.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import { AuthContext } from "./Axio";
+import useMessages from "../hooks/Message";
+import MessageList from "./MessageList";
+import MessageForm from "./MessageForm";
+
+export default function Chat() {
+  const { user } = useContext(AuthContext);
+  const { messages, sendMessage } = useMessages();
+
+  if (!user) return 
+  <p>Connectez-vous pour voir vos messages</p>;
+
+  return (
+    <div>
+      <h2>Messagerie</h2>
+      <MessageList messages={messages} currentUserId={user.id} />
+      <MessageForm onSend={sendMessage} />
+    </div>
+  );
+}
